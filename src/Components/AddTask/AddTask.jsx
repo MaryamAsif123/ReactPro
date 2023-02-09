82f16fe2f29ba298e73853e1264c9ea036c88a53
unknown <maryambutt260@gmail.com>
2023-02-09 22:43:17 +0200

    Bug Fixeddddd

--- src/Components/AddTask/AddTask.jsx
+++ src/Components/AddTask/AddTask.jsx
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import './AddTask.css'
+import React, { Component } from "react";
+import "./AddTask.css";
 class AddTask extends Component {
   state = {
-    inputText: '',
+    inputText: "",
   };
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
     this.props.ADD(this.state.inputText);
-    this.setState({ inputText: '' });
+    this.setState({ inputText: "" });
   };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input className='input'
+        <input
+          className="input"
           type="text"
           value={this.state.inputText}
           onChange={(event) => this.setState({ inputText: event.target.value })}
-        /> <br />
-        <button type="submit" className='btn'>Add Task</button>
+        />{" "}
+        <br />
+        <button type="submit" className="btn">
+          Add Task
+        </button>
       </form>
     );
   }
--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import Task from '../Task/Task';
+import React, { Component } from "react";
+import Task from "../Task/Task";
 
 class TaskList extends Component {
-    
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
   render() {
     return (
       <div>
         {this.props.tasks.map((item, index) => (
-          <Task 
-          key={index} 
-          showTask={item} 
-          deleteTask={this.props.deleteTask} 
-          index={index}
-          /> 
-        ))
-        }
+          <Task
+            key={index}
+            showTask={item}
+            deleteTask={this.props.deleteTask}
+            index={index}
+          />
+        ))}
       </div>
     );
   }
